aca42e21e3993a08ba620224163a783b4bd63409
leeyejin1231 <yejin@node01.toc.lab>
2025-02-27 11:08:06 +0900

    chagne config

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ import random
 from transformers import BertTokenizer
 from utils import NERTagger, get_dataloader, iter_product
 from model import CustomBERT
-from train_config import train_config
+import train_config
 from easydict import EasyDict as edict
 
 device = torch.device('cuda')

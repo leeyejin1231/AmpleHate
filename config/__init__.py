fbfce10e92c30556b58e3770f7cf08d2ef0c6617
leeyejin1231 <yejin@node01.toc.lab>
2025-02-27 11:05:44 +0900

    commit

--- config/__init__.py
+++
@@ -1,2 +0,0 @@
-from .train_config import *
-from .test_config import *
--- train.py
+++ train.py
@@ -9,7 +9,7 @@ import random
 from transformers import BertTokenizer
 from utils import NERTagger, get_dataloader, iter_product
 from model import CustomBERT
-import config as train_config
+from config import train_config
 from easydict import EasyDict as edict
 
 device = torch.device('cuda')

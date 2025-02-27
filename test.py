9c94aa845d00e53e87a0b737efa2c93cf52819b4
leeyejin1231 <yejin@node01.toc.lab>
2025-02-27 13:14:05 +0900

    [feat] add config

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ import random
 from transformers import BertTokenizer
 from utils import NERTagger, get_dataloader, iter_product
 from model import CustomBERT
-import test_config
+from config import test_config
 from easydict import EasyDict as edict
 
 device = torch.device('cuda')
